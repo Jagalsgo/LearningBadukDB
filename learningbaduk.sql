fe8c42c84ef0956a003f16aaffe5ac37e03244b5
Namix <ehdwns6781@naver.com>
2022-05-10 19:56:32 +0900

    update

--- learningbaduk.sql
+++ learningbaduk.sql
@@ -30,10 +30,11 @@ CREATE TABLE `board` (
   `boardCategory` varchar(45) NOT NULL,
   `boardReport` int NOT NULL DEFAULT '0',
   `userId` varchar(45) NOT NULL,
+  `boardHit` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`boardId`),
   KEY `FK_board_userId_user_userId` (`userId`),
   CONSTRAINT `FK_board_userId_user_userId` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,8 +43,31 @@ CREATE TABLE `board` (
 
 LOCK TABLES `board` WRITE;
 /*!40000 ALTER TABLE `board` DISABLE KEYS */;
+INSERT INTO `board` VALUES (1,'endGame1','endGame1','2022-05-10 19:17:30','endGame',0,'userId1',0);
 /*!40000 ALTER TABLE `board` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Temporary view structure for view `boardview`
+--
+
+DROP TABLE IF EXISTS `boardview`;
+/*!50001 DROP VIEW IF EXISTS `boardview`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `boardview` AS SELECT 
+ 1 AS `boardId`,
+ 1 AS `boardTitle`,
+ 1 AS `boardContent`,
+ 1 AS `boardDate`,
+ 1 AS `boardCategory`,
+ 1 AS `boardReport`,
+ 1 AS `userId`,
+ 1 AS `boardHit`,
+ 1 AS `commentCount`,
+ 1 AS `likeCount`,
+ 1 AS `dislikeCount`*/;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `comment`
@@ -63,7 +87,7 @@ CREATE TABLE `comment` (
   KEY `FK_comment_boardId_board_boardId` (`boardId`),
   CONSTRAINT `FK_comment_boardId_board_boardId` FOREIGN KEY (`boardId`) REFERENCES `board` (`boardId`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `FK_comment_userId_user_userId` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -72,7 +96,36 @@ CREATE TABLE `comment` (
 
 LOCK TABLES `comment` WRITE;
 /*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+INSERT INTO `comment` VALUES (1,'endGameComment1','2022-05-10 19:18:01','userId1',1);
 /*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dislikes`
+--
+
+DROP TABLE IF EXISTS `dislikes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dislikes` (
+  `dislikeId` int NOT NULL AUTO_INCREMENT,
+  `boardId` int NOT NULL,
+  `userId` varchar(45) NOT NULL,
+  PRIMARY KEY (`dislikeId`),
+  KEY `userId` (`userId`),
+  KEY `boardId` (`boardId`),
+  CONSTRAINT `dislikes_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`),
+  CONSTRAINT `dislikes_ibfk_2` FOREIGN KEY (`boardId`) REFERENCES `board` (`boardId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dislikes`
+--
+
+LOCK TABLES `dislikes` WRITE;
+/*!40000 ALTER TABLE `dislikes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dislikes` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -86,7 +139,6 @@ CREATE TABLE `likes` (
   `likeId` int NOT NULL AUTO_INCREMENT,
   `boardId` int NOT NULL,
   `userId` varchar(45) NOT NULL,
-  `likeCheck` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`likeId`),
   KEY `FK_like_userId_user_userId` (`userId`),
   KEY `FK_like_boardId_board_boardId` (`boardId`),
@@ -144,9 +196,9 @@ CREATE TABLE `user` (
   `userPassword` varchar(45) NOT NULL,
   `userNickname` varchar(45) NOT NULL,
   `userEmail` varchar(45) NOT NULL,
-  `userProfileImg` varchar(45) DEFAULT NULL,
+  `userProfileImg` varchar(80) DEFAULT NULL,
   `userReport` int NOT NULL DEFAULT '0',
-  `userRole` varchar(45) NOT NULL,
+  `userRole` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`userId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -157,8 +209,27 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('userId1','userPassword1','userNickname1','userEmail1',NULL,0,NULL);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Final view structure for view `boardview`
+--
+
+/*!50001 DROP VIEW IF EXISTS `boardview`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`namix`@`%` SQL SECURITY DEFINER */
+/*!50001 VIEW `boardview` AS select `board`.`boardId` AS `boardId`,`board`.`boardTitle` AS `boardTitle`,`board`.`boardContent` AS `boardContent`,`board`.`boardDate` AS `boardDate`,`board`.`boardCategory` AS `boardCategory`,`board`.`boardReport` AS `boardReport`,`board`.`userId` AS `userId`,`board`.`boardHit` AS `boardHit`,(select count(0) from `comment` where (`comment`.`boardId` = `board`.`boardId`)) AS `commentCount`,(select count(0) from `likes` where (`likes`.`boardId` = `board`.`boardId`)) AS `likeCount`,(select count(0) from `dislikes` where (`dislikes`.`boardId` = `board`.`boardId`)) AS `dislikeCount` from `board` */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -169,4 +240,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-04-25 21:07:20
+-- Dump completed on 2022-05-10 19:55:56
